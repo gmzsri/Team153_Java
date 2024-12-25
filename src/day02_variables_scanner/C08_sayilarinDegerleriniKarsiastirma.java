d96ea1a5f2b848617f648fd8835a9d1546dc8be9
gamze <gamzem14042001@gmail.com>
2024-12-25 10:57:38 +0300

    java eğitiminde 2.gün tamamlandı.

---
+++ src/day02_variables_scanner/C08_sayilarinDegerleriniKarsiastirma.java
@@ -0,0 +1,29 @@
+package day02_variables_scanner;
+
+import java.util.Scanner;
+
+public class C08_sayilarinDegerleriniKarsiastirma {
+    public static void main(String[] args) {
+
+        //kullanıcıdan iki sayı alıp ikisinin değerlerini değiştirin (swap)
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("lütfen birinci sayıyı giriniz");
+        int sayi1= scanner.nextInt();
+
+        System.out.println("lütfen ikinci sayıyı giriniz: ");
+        int sayi2= scanner.nextInt();
+
+        int temp = 0;
+
+        temp= sayi2;
+
+        sayi2= sayi1;
+
+        sayi1=temp;
+
+        System.out.println("son durum sayi1: "+ sayi1 + " son durum sayi2 :"+sayi2);
+
+    }
+}
