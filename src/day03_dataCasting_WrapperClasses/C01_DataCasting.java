008e488f6e7c44eeebaa90867a30566dc40a4246
gamze <gamzem14042001@gmail.com>
2025-02-22 13:38:25 +0300

    java eğitiminde 3.gün tamamlandı.

---
+++ src/day03_dataCasting_WrapperClasses/C01_DataCasting.java
@@ -0,0 +1,27 @@
+package day03_dataCasting_WrapperClasses;
+
+public class C01_DataCasting {
+    public static void main(String[] args) {
+
+        boolean b1= true;
+        char chr ='a';
+        byte byt = 24;
+        short shrt = 23;
+        int sayiInt = 30;
+        long lng= 40;
+        float flt = 3.5f;
+        double dbl = 4.7;
+        String str = "Java";
+
+        // boolean data türü sadece true ve false kanul eder başka hiçbir data türünü kabul etmez.
+
+        //daha küçük kapsamlı data türündeki variable'ı daha buyuk kapsamlı data türündeki variable'a atamak istediğimizde
+        // java otomatik olarak gerçekleştirir.
+
+        // Sırasıyla ;
+        //byte short int long float double
+
+
+
+    }
+}
